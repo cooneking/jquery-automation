13a12a4e579dd78da09132807abb1adf52d05cd7
cooneking <cooneking@gmail.com>
2020-06-11 15:34:37 -0400

    evenjan good

--- src/Day1/Goh.java
+++ src/Day1/Goh.java
@@ -3,5 +3,20 @@ package Day1;
 public class Goh {
     public static void main(String[] args) {
         System.out.println("Nuqak");
+
+    evenjan();
     }
+
+   public static void evenjan (){
+
+        int even = 100;
+        for(int i=0; i<even;i++){
+            if(i%2==1){
+                System.out.println(i);}
+
+        }
+
+
+   }
+
 }
